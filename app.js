91fea4175ff8aed5059853a5aaddac4320d69537
TTGOLE123 <F.Berges@gmx.de>
2025-10-01 14:07:19 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,2 @@
+const app = document.getElementById('app');
+app.innerHTML = '<h1>Cornhole PWA funktioniert!</h1><p>Die App ist geladen.</p>';
---
+++ service-worker.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', function(e){
+  e.waitUntil(caches.open('cornhole').then(cache => cache.addAll(['./','index.html','app.js','manifest.json'])));
+});
+self.addEventListener('fetch', function(e){
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
