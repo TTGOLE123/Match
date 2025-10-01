80cfe6890b9e95e7a397eab814da1b610317e6ad
TTGOLE123 <F.Berges@gmx.de>
2025-10-01 14:05:59 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,2 +0,0 @@
-self.addEventListener('install', function(e){e.waitUntil(caches.open('cornhole').then(cache=>cache.addAll(['./','index.html','app.js','manifest.json'])));});
-self.addEventListener('fetch', function(e){e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
